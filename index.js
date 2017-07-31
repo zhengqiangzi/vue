f396d48a8d843de27e2bb5a3cb902d7639767549
zhengqiangzi <zhengqiangzi123@163.com>
2017-07-31 21:41:54 +0800

    fixed

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-window.$=window.jQuery=require("jquery");
-require("bootstrap");
-import VueForm from 'vue-form';
 import Vue from 'vue';
+import Loading from 'element-ui/packages/loading'
 import 'element-ui/lib/theme-default/index.css'
-import { Table , tableColumn} from 'element-ui';
 
-import  Test  from "./components/test.component.vue";
-
-Vue.component('el-table',Table)
-Vue.component('el-table-column',tableColumn);
-Vue.component('my-tt',Test);
+console.log(Loading.service)
 
 var app = new Vue({
-
-  provide:{
-    user:{
-
-      username:"johnny",
-      age:30,
-      sex:1,
-      address:"shanghai"
-    }
-  },
-  el: '#app',
+  el:"#app",
   data:{
-     tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
+
+      a:2
   },
-  methods:{
+  mounted:function(){
 
 
-  },
-  components:{
+     let loading = Loading.service();
 
-
+     console.log(loading)
   }
- 
-
 })
